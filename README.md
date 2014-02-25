435749e419d47bebeef711091b31365517387764
wacky <wacky@kmc.gr.jp>
2014-02-25 21:10:21 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+This is a script to build unrealircd debian package.
+
+You need to install dh-make, devscripts, and autotools-dev
+in order to build debian package.
+
+Run build.sh, then you will get debian package.
+
+After you install this pacakge, you need to write
+your own /etc/unrealircd/unrealircd.conf.
+
+---
+
+これは unrealircd の deb パッケージを作るためのスクリプトです。
+
+deb パッケージを作るためには dh-make, devscripts, autotools-dev
+をインストールする必要があります。
+
+build.sh を実行することで、自動的にパッケージが作られます。
+
+インストール後は、 /etc/unrealircd/unrealircd.conf を自分で作成する必要があります。
---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+LANG=C
+pkgver=3.2.10.2
+
+[ ! -e Unreal$pkgver.tar.gz ] && wget http://www.unrealircd.com/downloads/Unreal$pkgver.tar.gz
+[ ! -e pkg ] && mkdir pkg
+cd pkg
+[ ! -e unrealircd-$pkgver.tar.gz ] && ln -s ../Unreal$pkgver.tar.gz unrealircd-$pkgver.tar.gz
+[ ! -e unrealircd_$pkgver.orig.tar.gz ] && ln -s ../Unreal$pkgver.tar.gz unrealircd_$pkgver.orig.tar.gz
+[ -e unrealircd-$pkgver ] && rm -rf unrealircd-$pkgver
+tar zxf unrealircd-$pkgver.tar.gz
+mv Unreal$pkgver unrealircd-$pkgver
+cd unrealircd-$pkgver
+cp -r ../../debian debian
+debuild -us -uc
+
+
