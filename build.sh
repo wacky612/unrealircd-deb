70c201b4bab63271be93041705fbff0f84051010
ErgoZ <ergozru@gmail.com>
2015-05-20 11:53:00 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 LANG=C
 pkgver=3.2.10.4
 
-[ ! -e Unreal$pkgver.tar.gz ] && wget https://www.unrealircd.org/downloads/Unreal$pkgver.tar.gz
+[ ! -e Unreal$pkgver.tar.gz ] && wget https://www.unrealircd.org/downloads/Unreal$pkgver.tar.gz --no-check-certificate
 [ ! -e pkg ] && mkdir pkg
 cd pkg
 [ ! -e unrealircd-$pkgver.tar.gz ] && ln -s ../Unreal$pkgver.tar.gz unrealircd-$pkgver.tar.gz
